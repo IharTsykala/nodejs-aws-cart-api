05f47f3bac9b6cc94e98fbc98a8cc6bf9be2ab8a
IharTsykala <ihatsykala@kitrum.com>
2023-07-09 22:54:26 +0600

    feat: add lambda cart service

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-
 import helmet from 'helmet';
+
+import { NestFactory } from '@nestjs/core';
+import serverlessExpress from "@vendia/serverless-express";
 
 import { AppModule } from './app.module';
 
-const port = process.env.PORT || 4000;
+// const port = process.env.PORT || 4000;
+
+let server
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,10 +15,24 @@ async function bootstrap() {
   app.enableCors({
     origin: (req, callback) => callback(null, true),
   });
+
   app.use(helmet());
 
-  await app.listen(port);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+
+  return serverlessExpress({ app: expressApp });
+
+  // await app.listen(port);
 }
-bootstrap().then(() => {
-  console.log('App is running on %s port', port);
-});
+// bootstrap().then(() => {
+//   console.log('App is running on %s port', port);
+// });
+
+
+export const handler = async (event: any, context: any, callback: any,) => {
+  server = server ?? (await bootstrap());
+
+  return server(event, context, callback);
+};
